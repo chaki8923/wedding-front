af97fec787a6ad16408b75dc8538419323dba10b
茶木 涼 <>
2024-06-21 19:07:46 +0900

    招待状

--- src/graphql/document.ts
+++ src/graphql/document.ts
@@ -27,12 +27,27 @@ export const POST_MESSAGE = gql`
 `;
 
 export const CREATE_INVITATION = gql`
-  mutation CreateInvitation($userId: String!, $title: String!, $event_date: String!, $place: String!) {
-    createInvitation(input: { userId: $userId, title: $title, event_date: $event_date, place: $place }) {
+  mutation CreateInvitation(
+    $userId: String!
+    $title: String!
+    $event_date: String!
+    $place: String!
+    $comment: String!
+  ) {
+    createInvitation(
+      input: {
+        userId: $userId
+        title: $title
+        event_date: $event_date
+        place: $place
+        comment: $comment
+      }
+    ) {
       id
       title
       event_date
       place
+      comment
       user {
         id
       }
@@ -41,10 +56,44 @@ export const CREATE_INVITATION = gql`
   }
 `;
 
+export const UPDATE_INVITATION = gql`
+  mutation UpdateInvitation(
+    $id: String!
+    $title: String
+    $event_date: String
+    $place: String
+    $comment: String
+  ) {
+    updateInvitation(
+      input: { id: $id, title: $title, event_date: $event_date, place: $place, comment: $comment }
+    ) {
+      id
+      title
+      event_date
+      place
+      comment
+    }
+  }
+`;
 
 export const GET_INVITATION = gql`
   query GetInvitation {
     getInvitation {
+      id
+      title
+      event_date
+      place
+      created_at
+      user {
+        name
+      }
+    }
+  }
+`;
+
+export const SHOW_INVITATION = gql`
+  query ShowInvitation($id: string) {
+    showInvitation(id: $id) {
       id
       title
       event_date

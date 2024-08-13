c2064b11bde8a187184c0fee7ba20a1aab7d1782
soratenchan <54834092+soratenchan@users.noreply.github.com>
2024-08-13 19:04:22 +0900

    メールデザイン

--- src/components/Mail/presenter.tsx
+++ src/components/Mail/presenter.tsx
@@ -1,4 +1,3 @@
-import styles from './index.module.scss';
 import { SendMail } from '@/types/form';
 import { FieldErrors, SubmitHandler, UseFormHandleSubmit, UseFormRegister } from 'react-hook-form';
 
@@ -9,53 +8,58 @@ type Props = {
   errors: FieldErrors<SendMail>;
 };
 
-
 export function Presenter(props: Props) {
-
   return (
     <>
-      <form className={styles.loginForm} onSubmit={props.handleSubmit(props.onSubmit)}>
-        <div className={styles.formWrapper}>
-          <div>メール送信</div>
-          <div className={styles.error}>
-            {props.errors.subject && <span>subject is required</span>}
-            {props.errors.to && <span>to is required</span>}
-            {props.errors.from && <span>from is required</span>}
-            {props.errors.body && <span>body is required</span>}
-          </div>
-          <div className={styles.inputWrapper}>
+      <form className="mt-36 font-serif" onSubmit={props.handleSubmit(props.onSubmit)}>
+        <div className="flex flex-col items-center">
+          <div className="text-gray-600 text-xl pb-12">メール送信</div>
+          <div className="flex flex-col items-center w-full max-w-sm">
             <input
-              type='text'
-              placeholder='to'
+              type="text"
+              placeholder="To"
+              className="w-full p-3 mb-4 bg-transparent text-base border-b border-gray-400 outline-none"
               {...props.register('to', {
-                required: false
+                required: false,
               })}
             />
+            {props.errors.to && <span className="text-red-500 text-xs mb-4 w-full">Toは必須です</span>}
+
             <input
-              type='text'
-              placeholder='from'
+              type="text"
+              placeholder="From"
+              className="w-full p-3 mb-4 bg-transparent text-base border-b border-gray-400 outline-none"
               {...props.register('from', {
                 required: false,
                 pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
               })}
             />
+            {props.errors.from && <span className="text-red-500 text-xs mb-4 w-full">Fromは必須です</span>}
+
             <input
-              type='text'
-              placeholder='subject'
+              type="text"
+              placeholder="Subject"
+              className="w-full p-3 mb-4 bg-transparent text-base border-b border-gray-400 outline-none"
               {...props.register('subject', {
-                required: true
+                required: true,
               })}
             />
+            {props.errors.subject && <span className="text-red-500 text-xs mb-4 w-full">Subjectは必須です</span>}
+
             <input
-              type='text'
-              placeholder='招待状uuid'
+              type="text"
+              placeholder="招待状ID"
+              className="w-full p-3 mb-4 bg-transparent text-base border-b border-gray-400 outline-none"
               {...props.register('body', {
-                required: true
+                required: true,
               })}
             />
-            
+            {props.errors.body && <span className="text-red-500 text-xs mb-4 w-full">招待状IDは必須です</span>}
           </div>
-          <button className={styles.submitBtn} type='submit'>
+          <button
+            className="p-3 px-12 tracking-wide mt-4 border-gray-400 border-2 bg-white text-base rounded-md cursor-pointer hover:bg-gray-300"
+            type="submit"
+          >
             送信
           </button>
         </div>

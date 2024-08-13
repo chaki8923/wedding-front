32381442369e3ae8c341efcc0857c0724bccfb97
soratenchan <54834092+soratenchan@users.noreply.github.com>
2024-08-13 19:01:43 +0900

    ホットリロード対応

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

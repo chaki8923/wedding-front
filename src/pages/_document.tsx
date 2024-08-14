2cec62457ef3ca754e80ff42d6f3523c31aed4a0
soratenchan <54834092+soratenchan@users.noreply.github.com>
2024-08-14 19:19:43 +0900

    コメント追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        {/* 一旦cdn */}
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <body>

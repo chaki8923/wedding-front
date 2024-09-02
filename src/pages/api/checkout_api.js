b305dec80013c8ed4c5f14af99b9ee49860189ef
茶木 涼 <>
2024-09-02 09:32:27 +0900

    うりゃ！2

--- src/pages/api/checkout_api.js
+++ src/pages/api/checkout_api.js
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
-console.log("key!!!!-----------------", process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+console.log("key!!!!-----------------", process.env.STRIPE_SECRET_KEY);
 
-export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16"
 })
 

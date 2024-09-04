f802478fdc269971b6459760a818017ab449e7f2
kyyk5447 <w140113@gmail.com>
2024-09-05 00:46:48 +0900

    ログイン画面マークアップ修正

--- src/components/Login/useLogin.ts
+++ src/components/Login/useLogin.ts
@@ -2,7 +2,7 @@ import { useUserState } from '@/atoms/userAtom'
 import { useSetCsrf } from '@/components/Login/useSetCsrf'
 import { Login } from '@/types/form'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect, useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useCookies } from 'react-cookie'
 
 export const useLogin = () => {
@@ -11,6 +11,7 @@ export const useLogin = () => {
   const router = useRouter()
   const params = useMemo(() => new URLSearchParams(), [])
   const [cookies, setUseCookies] = useCookies(['_csrf'])
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   useEffect(() => {
     setUseCookies('_csrf', cookies._csrf)
@@ -18,8 +19,8 @@ export const useLogin = () => {
 
   const login = useCallback(
     async (Inputs: Login) => {
+      setIsSubmitting(true)
       await setCsrf()
-
       params.append('email', Inputs.email)
       params.append('password', Inputs.password)
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
@@ -34,16 +35,18 @@ export const useLogin = () => {
         .then((response) => response.json())
         .then((data) => {
           setUser(data)
+          setIsSubmitting(false)
           document.cookie = `weddingUserId=${data.userId}; path=/; max-age=2592000; SameSite=Strict; Secure`
           router.push('/timeLine')
         })
         .catch((error) => {
           setUser(null)
+          setIsSubmitting(false)
           router.push('/')
         })
     },
     [cookies._csrf, params, router, setCsrf, setUser]
   )
 
-  return { login }
+  return { login, isSubmitting }
 }

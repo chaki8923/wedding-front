51c41b23845fe23f6906a38260b91ea28a460156
soratenchan <54834092+soratenchan@users.noreply.github.com>
2024-09-05 13:41:24 +0900

    修正

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -2,7 +2,7 @@ import { Noto_Sans_JP } from 'next/font/google'
  
 const notoSansJP = Noto_Sans_JP({
   preload: false,
-  weight: ['400','500','700'], // 必要な重さを選択
+  weight: ['400','500','700'],
   subsets: ['latin'],
   variable: '--font-noto-sans-jp',
   display: 'swap',
